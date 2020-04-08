a0da3ee79ffb395ace310f8347ef14bedc48a593
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-08 18:56:21 -0400

    removed webpack

--- index.js
+++ index.js
@@ -13,4 +13,19 @@ const initUpdateNavbarOnScroll = () => {
 		});
 	}
 };
-export { initUpdateNavbarOnScroll };
+initUpdateNavbarOnScroll();
+
+const transform = () => {
+	const button = document.querySelector('.btn-custom');
+	const arrow = document.querySelector('.fa-arrow-right');
+	button.addEventListener('mouseenter', () => {
+		arrow.style.transition = 'all 0.5s';
+		arrow.style.transform = 'rotate(90deg)';
+	});
+	button.addEventListener('mouseleave', () => {
+		arrow.style.transition = 'all 0.5s';
+		arrow.style.transform = 'rotate(0deg)';
+	});
+};
+
+transform();
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import _ from './components/navBar';
-import { loadDynamicBannerText } from './components/typed';
-import { transform } from './components/transform';
-import { initUpdateNavbarOnScroll } from './components/navBar';
-initUpdateNavbarOnScroll();
-transform();
