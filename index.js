d4aff17366435a6155bb9f6811cab77cb075a2c7
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-09 13:36:43 -0400

    made navlinks transparent

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const initUpdateNavbarOnScroll = () => {
 	const navbar = document.querySelector('.navbar-lewagon');
 	const toggler = document.querySelector('.navbar-toggler');
+	const navlinks = document.querySelectorAll('.nav-links');
 	if (navbar) {
 		window.addEventListener('scroll', () => {
 			if (window.scrollY >= window.innerHeight) {
 				navbar.classList.add('navbar-lewagon-white');
 				toggler.classList.add('navbar-toggler-white');
+				for (let navlink of navlinks) {
+					navlink.classList.remove('nav-links');
+					navlink.classList.add('nav-link');
+				}
 			} else {
 				navbar.classList.remove('navbar-lewagon-white');
 				toggler.classList.remove('navbar-toggler-white');
+				for (let navlink of navlinks) {
+					navlink.classList.remove('nav-link');
+					navlink.classList.add('nav-links');
+				}
 			}
 		});
 	}
